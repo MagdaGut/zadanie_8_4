722b2eb6f2f9249cd92e43e2a5d79b31e10739f7
Magda Gutowska <m.gutowska0909@gmail.com>
2018-03-17 22:18:06 +0100

    Add three comment

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+// scripts.js
+
+var name = prompt('Enter your name');		  //okno do wpisania imienia
+alert('Hello, ' + name);                      //wyskakuje okno
+console.log('Hello, ' + name);				  //powitanie przez stronę 
